37c8c1d8bb9b83570479b8f60659c1bfd045cdf8
Uvindu Wijesinghe <uvinduwijesinghe@hotmail.com>
2019-04-17 20:58:31 +0100

    Added files:  - dataset_simulation - which shows predicted vs actual steering angle
    Signed-off-by: Uvindu Wijesinghe <uvinduwijesinghe@hotmail.com>

--- dataset_playback.py
+++ dataset_playback.py
@@ -1,13 +1,9 @@
-from model import network_model
 import numpy as np
 import glob
 import cv2
 
-cnn_model = network_model()
-cnn_model.load_weights("drive_model_44.h5")
-img = cv2.imread('steering_wheel_image.jpg',0)
-img_actual = cv2.imread('steering_wheel_image.jpg',0)
-rows,cols = img.shape
+img_actual = cv2.imread('steering_wheel_image.jpg', 0)
+rows, cols = img_actual.shape
 smoothed_angle = 0
 smoothed_angle_actual = 0
 
@@ -20,15 +16,8 @@ image = np.empty([1, 240, 320, 3])
 for filename in glob.glob(folder_name + "*.jpg"):
     image[0] = cv2.imread(filename)
 
-    cv2.imshow("Driving Footage", image[0,-150:, :, :]/255)
-    # Predict angle
-    degrees = cnn_model.predict(image)[0] * 180.0
+    cv2.imshow("Driving Footage", image[0, :, :, :] / 255)
 
-    # Smooth angle for animated steering wheel
-    smoothed_angle += 0.2 * pow(abs((degrees - smoothed_angle)), 2.0 / 3.0) * (degrees - smoothed_angle) / abs(degrees - smoothed_angle)
-    M = cv2.getRotationMatrix2D((cols/2,rows/2),-smoothed_angle,1)
-    dst = cv2.warpAffine(img,M,(cols,rows))
-    cv2.putText(dst, "Predicted: " + str(round(int(degrees), 2)), (int(cols/2 - 90), 40), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), lineType=cv2.LINE_AA)
     # Get actual angle from file name
     command = filename[len(folder_name) + 24:48]
     angle = int(command[1:4])
@@ -40,13 +29,14 @@ for filename in glob.glob(folder_name + "*.jpg"):
         angle = 0.1
 
     # Smooth angle for animated steering wheel
-    smoothed_angle_actual += 0.2 * pow(abs((angle - smoothed_angle_actual)), 2.0 / 3.0) * (angle - smoothed_angle_actual) / abs(angle - smoothed_angle_actual)
-    M_actual = cv2.getRotationMatrix2D((cols/2,rows/2),-smoothed_angle_actual,1)
-    dst_actual = cv2.warpAffine(img_actual,M_actual,(cols,rows))
-    cv2.putText(dst_actual, "Actual: " + str(round(angle, 0)), (int(cols / 2 - 70), 40), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255),
+    smoothed_angle_actual += 0.2 * pow(abs((angle - smoothed_angle_actual)), 2.0 / 3.0) * (
+                angle - smoothed_angle_actual) / abs(angle - smoothed_angle_actual)
+    M_actual = cv2.getRotationMatrix2D((cols / 2, rows / 2), -smoothed_angle_actual, 1)
+    dst_actual = cv2.warpAffine(img_actual, M_actual, (cols, rows))
+    cv2.putText(dst_actual, "Actual: " + str(round(angle, 0)), (int(cols / 2 - 70), 40), cv2.FONT_HERSHEY_SIMPLEX, 1.0,
+                (255, 255, 255),
                 lineType=cv2.LINE_AA)
-    # Display the two steering wheels
-    cv2.imshow("Predicted steering wheel", dst)
+    # Display the steering wheels
     cv2.imshow("Actual steering wheel", dst_actual)
 
     cv2.waitKey(10)
